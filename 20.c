721295b2357a906706983491b995cdb3b3c6142d
DEVIPRIYAS <30364226+DEVIPRIYAS@users.noreply.github.com>
2017-08-07 15:48:28 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a, j;
+
+    printf("Enter an integer: ");
+    scanf("%d",&a);
+    for(j=1; j<=10; ++j)
+    {
+        printf("%d * %d = %d \n", a, j, a*i);
+    }
+    return 0;
+}

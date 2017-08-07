2ee4a1a76d0b7cdabf9a1953bace08f71fb8def4
DEVIPRIYAS <30364226+DEVIPRIYAS@users.noreply.github.com>
2017-08-07 14:57:15 +0530

    Create 14.c

---
+++ 14.c
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int e;
+ int g;
+ cout<<" enter number "<<endl;
+ cin>>e;
+ cout<<" enter number "<<endl;
+ cin>>g;
+ cout<<"***************************"<<endl;
+ cout<<" programology student site "<<endl;
+ cout<<"***************************"<<endl;
+ for(;e<=g;)
+ {
+ cout<<e<<endl;
+ e=e+2;
+ }
+}
